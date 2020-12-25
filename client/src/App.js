f11d820e849d59f8cdd13458e00fbd6230bead05
group20RNS <group20finalproject@gmail.com>
2020-12-25 22:03:13 +0530

    25/12/9-53

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+import DisplayHello from "./components/displayhello-image.component";
+import HomeScreen from "./components/Home-screening.component";
+
+function App() {
+  return (
+  
+    <Router>
+      <div className="container">
+        <Navbar />
+        <br />
+        <Route path="/" exact component={HomeScreen} />
+        <Route path="/posts" component={ExercisesList} />
+        <Route path="/smovies" component={CreateExercise} />
+        <Route path="/srls" component={CreateUser} />
+        <Route path="/mvs" component={DisplayHello} />
+        <Route path="/el" component={EditExercise} />
+            
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/components/navbar.component.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+    
+       
+      
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <Link to="/" className="navbar-brand">
+         BEE CREATIVE STUDIOS
+        </Link>
+       
+        <div className="collpase navbar-collapse flexible">
+          <br></br>
+          <ul className="navbar-nav mr-auto">
+            <li className="navbar-item">
+
+              <Link to="/posts" className="nav-link">
+               POSTERS
+              </Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/smovies" className="nav-link">
+                SHORT MOVIES
+              </Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/srls" className="nav-link">
+                SERIALS
+              </Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/mvs" className="nav-link">
+                MOVIES
+              </Link>
+            </li>
+         
+          </ul>
+        </div>
+      </nav>
+     
+    );
+  }
+}
